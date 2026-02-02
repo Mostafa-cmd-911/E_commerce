60b7ab3b4285ac78ce8d421a2bd0d698f5c50411
Mostafa-cmd-911 <mostafaalibeduar@gmail.com>
2026-02-02 22:55:55 +0200

    edit cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = 3000;
 
 const allowedOrigins = [
-    "http://127.0.0.1:5500",
+    "http://localhost:5173/",
     "https://unmeasuredly-unplentiful-lynsey.ngrok-free.dev",
 ];
 
